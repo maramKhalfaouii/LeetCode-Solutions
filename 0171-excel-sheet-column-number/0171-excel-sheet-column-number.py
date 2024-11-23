ec0e38005ce28d32b4b9918fb767b02a5d983aa4
Maram Khalfaoui <128645875+maramKhalfaouii@users.noreply.github.com>
2024-11-23 12:09:48 +0100

    Time: 0 ms (100%), Space: 11.5 MB (65.04%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def titleToNumber(self, columnTitle):
-        res = 0
-        for ch in columnTitle:
-            res = res * 26 + ord(ch)-ord('A')+1
-        return res     
+        # res = 0
+        # for ch in columnTitle:
+        #     res = res * 26 + ord(ch)-ord('A')+1
+        # return res     
+        return functools.reduce(lambda total, ch:total*26+ord(ch)-ord('A')+1,columnTitle, 0)
