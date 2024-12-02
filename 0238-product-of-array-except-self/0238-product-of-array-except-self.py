a2c2e3e5aee1c0cc5eca6b30133430612692f6b7
Maram Khalfaoui <128645875+maramKhalfaouii@users.noreply.github.com>
2024-12-03 00:47:49 +0100

    Time: 27 ms (92.06%), Space: 20.1 MB (45.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,22 +1,34 @@
 class Solution(object):
     def productExceptSelf(self, nums):
         n = len(nums)
-        pref = [1] * n
-        post = [1] * n
-        pref[0]=nums[0]
-        for i in range (1,n):
-            pref[i]=pref[i-1]*nums[i]
-        post[n-1]=nums[n-1]
-        for i in range (n-2,-1,-1):
-            post[i]=post[i+1]*nums[i]
-        for i in range(n):
-            if i==0: 
-                nums[i]=post[i+1]
-            elif i==n-1:
-                nums[i]=pref[i-1]
-            else:
-                nums[i]=pref[i-1]*post[i+1]
-        return nums
-        
+        # pref = [1] * n
+        # post = [1] * n
+        # pref[0]=nums[0]
+        # for i in range (1,n):
+        #     pref[i]=pref[i-1]*nums[i]
+        # post[n-1]=nums[n-1]
+        # for i in range (n-2,-1,-1):
+        #     post[i]=post[i+1]*nums[i]
+        # for i in range(n):
+        #     if i==0: 
+        #         nums[i]=post[i+1]
+        #     elif i==n-1:
+        #         nums[i]=pref[i-1]
+        #     else:
+        #         nums[i]=pref[i-1]*post[i+1]
+        # return nums
+#for o(1) space complexity :
+        res=[1]*n
+        prefix=1
+        for i in range (n):
+            res[i]=prefix
+            prefix*=nums[i]
+        postfix=1
+        for i in range(n-1,-1,-1):
+            res[i]*=postfix
+            postfix*=nums[i]
+        return res
+
+
 
         
