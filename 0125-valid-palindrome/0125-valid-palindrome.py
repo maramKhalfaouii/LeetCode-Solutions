bb91419fb11b8196c205e3252ff15f06d1568ee8
Maram Khalfaoui <128645875+maramKhalfaouii@users.noreply.github.com>
2024-12-06 19:28:55 +0100

    Time: 7 ms (96.41%), Space: 13.5 MB (43%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,11 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        newString=""
         if len(s)==1:
             return True
-        for c in s:
-            if ord(lower(c))>=ord('a') and ord(lower(c))<=ord('z') and c!=" " or c.isnumeric()  :
-                newString+=lower(c)
-        return newString==newString[::-1]
+        s = s.lower()
+        # for c in s:
+        #     if (ord(c)<ord('a') or ord(c)>ord('z')) and not c.isnumeric() :
+        #         s = s.replace(c,'')
+        a = ''.join(char for char in s if char.isalnum())
+        return a==a[::-1]
         
