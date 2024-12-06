8c87891c9d0d67921d54cdc2bfcd9959cfb7330d
Maram Khalfaoui <128645875+maramKhalfaouii@users.noreply.github.com>
2024-12-06 19:16:19 +0100

    Time: 179 ms (24.72%), Space: 12.7 MB (70.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -10,12 +10,5 @@ class Solution(object):
         for c in s:
             if ord(lower(c))>=ord('a') and ord(lower(c))<=ord('z') and c!=" " or c.isnumeric()  :
                 newString+=lower(c)
-        i,j=0,len(newString)-1
-        while i<j:
-            if newString[i]==newString[j]:
-                i+=1
-                j-=1
-            else: 
-                return False
-        return True
+        return newString==newString[::-1]
         
